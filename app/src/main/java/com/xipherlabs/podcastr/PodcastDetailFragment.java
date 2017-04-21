62eeca5f8801ff85637e70df94d649765f74b6d4
Tejas Bhole <Bhole.Tejas@live.com>
2017-04-21 07:58:09 +0530

    Checkpoint 7

--- app/src/main/java/com/xipherlabs/podcastr/PodcastDetailFragment.java
+++ app/src/main/java/com/xipherlabs/podcastr/PodcastDetailFragment.java
@@ -3,10 +3,12 @@ package com.xipherlabs.podcastr;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -46,37 +48,16 @@ public class PodcastDetailFragment extends Fragment {
         //Toast.makeText(getContext(),"Did it!",Toast.LENGTH_LONG).show();
 
         ImageView img = (ImageView) view.findViewById(R.id.detail_image);
-        ImageView img1 = (ImageView) view.findViewById(R.id.detail_image1);
-        ImageView img2 = (ImageView) view.findViewById(R.id.detail_image2);
-        ImageView img3 = (ImageView) view.findViewById(R.id.detail_image3);
-        ImageView img4 = (ImageView) view.findViewById(R.id.detail_image4);
-
+        TextView tv = (TextView) view.findViewById(R.id.detail_name);
+        tv.setText(podcast.getId());
         String imageUrl = podcast.getThumb();
         Glide.with(getContext())
                 .load(podcast.getThumb().replace("170x170","600x600"))
                 .skipMemoryCache(false)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
                 .into(img);
-        Glide.with(getContext())
-                .load(podcast.getThumb().replace("170x170","600x600"))
-                .skipMemoryCache(false)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(img1);
-        Glide.with(getContext())
-                .load(podcast.getThumb().replace("170x170","600x600"))
-                .skipMemoryCache(false)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(img2);
-        Glide.with(getContext())
-                .load(podcast.getThumb())
-                .skipMemoryCache(false)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(img3);
-        Glide.with(getContext())
-                .load(podcast.getThumb().replace("170x170","200X200"))
-                .skipMemoryCache(false)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .into(img4);
+        //Log.d("DetailFrag",""+podcast.getId());
         return view;
     }
 }
